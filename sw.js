57688e08928dffce155cae84455c783bfa80cb7a
emanos994-lab <emanos994@gmail.com>
2026-09-23 00:10:26 +0300

    Refresh MANOS DRIVE 3.1 cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='manos-drive-v5';
+const CACHE='manos-drive-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','https://upload.wikimedia.org/wikipedia/commons/f/fb/VW_Tiguan_front_20071228.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(async c=>{for(const a of ASSETS){try{await c.add(a)}catch(_){}}}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
